29c538c667561a99caa90f2f368ebefabfd84090
sangeeta <sangeeta@dvlp-HVM-domU>
2014-04-21 16:56:35 +0530

    committing ManagerDB

---
+++ empproject/src/main/java/com/mind/project/shared/model/ManagerDB.java
@@ -0,0 +1,24 @@
+package com.mind.project.shared.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ManagerDB {
+	
+	public Map<Integer,List<Integer> > mgr = new HashMap<Integer,List<Integer> >();
+	
+	
+	public void addReportees(Integer mgr_id, List<Integer> list)
+	{
+		mgr.put(mgr_id, list);
+	}
+	
+    public int getMapCount()
+    {
+    	System.out.println("The size of the manager map is" +mgr.size());
+    	return mgr.size();
+    }
+	
+
+}
